6c29bc452a0d39666b722d25968cbae92a2f0d18
Rafiul-goni-rayhan <rayhan.cse8.bu@gmail.com>
2026-05-01 19:14:00 +0600

    navbar complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Navbar from './../components/Navbar';
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
     <div className="">
-      
+     <Banner/>
+
     </div>
   );
 }
